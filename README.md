49770f8a0c6387041e581b9f7edfd059260ad765
JimZhou <jim.zhou@uvanart.com>
2019-09-08 14:17:56 +0800

    修正

---
+++ README.md
@@ -0,0 +1,8 @@
+### 网易方案
+
+此方案为网易的方案，也是最简单的方案，去除了淘宝方案的边界处理和兼容处理
+
+由于没有动态设置meta所以要在head中引入
+```html
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+```
---
+++ js/simple-flexible.js
@@ -0,0 +1,15 @@
+// 网易rem等比放大方案 
+// initial-scale=1.0的情况下，PC端的chrome浏览器调试最低字体是12px，移动端能更小 
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = clientWidth / 10 + 'px';
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
